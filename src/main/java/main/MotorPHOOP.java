b4b6b906d03d8552e8fed41f94812d17399f4f7f
kleinborre <131511662+kleinborre@users.noreply.github.com>
2025-03-10 23:57:03 +0800

    First Commit. Created directories for classes, databases, jframes, and main.

---
+++ src/main/java/main/MotorPHOOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package main;
+
+import jframes.*;
+/**
+ *
+ * @author STUDY MODE
+ */
+public class MotorPHOOP {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
